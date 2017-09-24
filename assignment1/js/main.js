188d904a30f45ce1d26a4130ee80d451a5904692
Stephanie <stran40@myseneca.ca>
2017-09-24 18:44:40 -0400

    Added node-sass.

--- assignment1/js/main.js
+++ assignment1/js/main.js
@@ -4,7 +4,7 @@
  * No part of this assignment has been copied manually or electronically from any other source
  * (including web sites) or distributed to other students.
  *
- * Name: Stephanie Tran Student ID: 134 233 162 Date: September 15 2017
+ * Name: Stephanie Tran Student ID: 134 233 162 Date: September 24 2017
  * Heroku link: https://glacial-cove-51366.herokuapp.com/
  *
  *
