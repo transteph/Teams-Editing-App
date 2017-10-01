4983e176766b724ce9a1abd57959d9eab64a8fdb
Stephanie <stran40@myseneca.ca>
2017-10-01 14:03:59 -0400

    Changed data queries

--- server.js
+++ server.js
@@ -9,201 +9,231 @@ const dataService = require("./data-service.js");
 
 const data = dataService(mongoDBConnectionString);
 const app = express();
-const path = require('path');
-
-//setup static folder
-app.use(express.static("assignment2"));
 
 app.use(bodyParser.json());
 app.use(cors());
 
-// setup a 'route' to listen on the default url path (http://localhost)
-app.get("/", function (req, res) {
-    try {
-        res.sendFile(path.join(_dirname + "index.html"));
-    } catch (rejectMsg) {
-        // catch any errors here
-        console.log(rejectMsg);
-    };
-});
-
 // "Employee" Routes
 
-app.get("/employees", (req,res) => {
-    data.getAllEmployees().then((data)=>{
-        res.json(data);
-    })
-    .catch((err)=>{
-        res.status(500).end();
-    })
+app.get("/employees", (req, res) => {
+    data.getAllEmployees().then((data) => {
+            res.json(data);
+        })
+        .catch((err) => {
+            res.status(500).end();
+        })
 });
 
-app.get("/employee/:employeeId", (req,res) => {
-    data.getEmployeeById(req.params.employeeId).then((data)=>{
-        if(data.length > 0){
+app.get("/employees-raw", (req, res) => {
+    data.getAllEmployeesRaw().then((data) => {
             res.json(data);
-        }else{
-            res.status(404).end();
-        }
-    })
-    .catch((err)=>{
-        res.status(500).end();
-    })
+        })
+        .catch((err) => {
+            res.status(500).end();
+        })
+});
+
+app.get("/employee/:employeeId", (req, res) => {
+    data.getEmployeeById(req.params.employeeId).then((data) => {
+            if (data.length > 0) {
+                res.json(data);
+            } else {
+                res.status(404).end();
+            }
+        })
+        .catch((err) => {
+            res.status(500).end();
+        })
+});
+
+app.get("/employee-raw/:employeeId", (req, res) => {
+    data.getEmployeeByIdRaw(req.params.employeeId).then((data) => {
+            if (data.length > 0) {
+                res.json(data);
+            } else {
+                res.status(404).end();
+            }
+        })
+        .catch((err) => {
+            res.status(500).end();
+        })
 });
 
 app.put("/employee/:employeeId", (req, res) => {
 
-    data.updateEmployeeById(req.params.employeeId, req.body).then((data)=>{
-        res.json({"message": "Employee " + data + " updated successfully"});
-    })
-    .catch((err)=>{
-        res.status(500).end();
-    })
+    data.updateEmployeeById(req.params.employeeId, req.body).then((data) => {
+            res.json({ "message": "Employee " + data + " updated successfully" });
+        })
+        .catch((err) => {
+            res.status(500).end();
+        })
 });
 
 app.post("/employees", (req, res) => {
-    
-    data.addEmployee(req.body).then((data)=>{
-        res.json({"message": "Employee " + data + " added successfully"});
-    })
-    .catch((err)=>{
-        res.status(500).end();
-    })
+
+    data.addEmployee(req.body).then((data) => {
+            res.json({ "message": "Employee " + data + " added successfully" });
+        })
+        .catch((err) => {
+            res.status(500).end();
+        })
 });
 
 ////////////////////
 
 // "Position" Routes
 
-app.get("/positions", (req,res) => {
-    data.getAllPositions().then((data)=>{
-        res.json(data);
-    })
-    .catch((err)=>{
-        res.status(500).end();
-    })
-});
-
-app.get("/position/:positionId", (req,res) => {
-    data.getPositionById(req.params.positionId).then((data)=>{
-        if(data.length > 0){
+app.get("/positions", (req, res) => {
+    data.getAllPositions().then((data) => {
             res.json(data);
-        }else{
-            res.status(404).end();
-        }
-    })
-    .catch((err)=>{
-        res.status(500).end();
-    })
+        })
+        .catch((err) => {
+            res.status(500).end();
+        })
 });
 
-app.put("/position/:positionId", (req,res) => {
-    data.updatePositionById(req.params.positionId, req.body).then((data)=>{
-        res.json({"message": "Position " + data + " updated successfully"});
-    })
-    .catch((err)=>{
-        res.status(500).end();
-    })
+app.get("/position/:positionId", (req, res) => {
+    data.getPositionById(req.params.positionId).then((data) => {
+            if (data.length > 0) {
+                res.json(data);
+            } else {
+                res.status(404).end();
+            }
+        })
+        .catch((err) => {
+            res.status(500).end();
+        })
+});
+
+app.put("/position/:positionId", (req, res) => {
+    data.updatePositionById(req.params.positionId, req.body).then((data) => {
+            res.json({ "message": "Position " + data + " updated successfully" });
+        })
+        .catch((err) => {
+            res.status(500).end();
+        })
 });
 
 app.post("/positions", (req, res) => {
-    
-    data.addPosition(req.body).then((data)=>{
-        res.json({"message": "Position " + data + " added successfully"});
-    })
-    .catch((err)=>{
-        res.status(500).end();
-    })
+
+    data.addPosition(req.body).then((data) => {
+            res.json({ "message": "Position " + data + " added successfully" });
+        })
+        .catch((err) => {
+            res.status(500).end();
+        })
 });
 
 ////////////////////
 
 // "Project" Routes
 
-app.get("/projects", (req,res) => {
-    data.getAllProjects().then((data)=>{
-        res.json(data);
-    })
-    .catch((err)=>{
-        res.status(500).end();
-    })
-});
-
-app.get("/project/:projectId", (req,res) => {
-    data.getProjectById(req.params.projectId).then((data)=>{
-        if(data.length > 0){
+app.get("/projects", (req, res) => {
+    data.getAllProjects().then((data) => {
             res.json(data);
-        }else{
-            res.status(404).end();
-        }
-    })
-    .catch((err)=>{
-        res.status(500).end();
-    })
+        })
+        .catch((err) => {
+            res.status(500).end();
+        })
 });
 
-app.put("/project/:projectId", (req,res) => {
-    data.updateProjectById(req.params.projectId, req.body).then((data)=>{
-        res.json({"message": "Project " + data + " updated successfully"});
-    })
-    .catch((err)=>{
-        res.status(500).end();
-    })
+app.get("/project/:projectId", (req, res) => {
+    data.getProjectById(req.params.projectId).then((data) => {
+            if (data.length > 0) {
+                res.json(data);
+            } else {
+                res.status(404).end();
+            }
+        })
+        .catch((err) => {
+            res.status(500).end();
+        })
+});
+
+app.put("/project/:projectId", (req, res) => {
+    data.updateProjectById(req.params.projectId, req.body).then((data) => {
+            res.json({ "message": "Project " + data + " updated successfully" });
+        })
+        .catch((err) => {
+            res.status(500).end();
+        })
 });
 
 app.post("/projects", (req, res) => {
-    
-    data.addProject(req.body).then((data)=>{
-        res.json({"message": "Project " + data + " added successfully"});
-    })
-    .catch((err)=>{
-        res.status(500).end();
-    })
+
+    data.addProject(req.body).then((data) => {
+            res.json({ "message": "Project " + data + " added successfully" });
+        })
+        .catch((err) => {
+            res.status(500).end();
+        })
 });
 
 ////////////////////
 
 // "Team Routes"
 
-app.get("/teams", (req,res) => {
-    data.getAllTeams().then((data)=>{
-        res.json(data);
-    })
-    .catch((err)=>{
-        res.status(500).end();
-    })
-});
-
-app.get("/team/:teamId", (req,res) => {
-    data.getTeamById(req.params.teamId).then((data)=>{
-        if(data.length > 0){
+app.get("/teams", (req, res) => {
+    data.getAllTeams().then((data) => {
             res.json(data);
-        }else{
-            res.status(404).end();
-        }
-    })
-    .catch((err)=>{
-        res.status(500).end();
-    })
+        })
+        .catch((err) => {
+            res.status(500).end();
+        })
 });
 
-app.put("/team/:teamId", (req,res) => {
-    data.updateTeamById(req.params.teamId, req.body).then((data)=>{
-        res.json({"message": "Team " + data + " updated successfully"});
-    })
-    .catch((err)=>{
-        res.status(500).end();
-    })
+app.get("/teams-raw", (req, res) => {
+    data.getAllTeamsRaw().then((data) => {
+            res.json(data);
+        })
+        .catch((err) => {
+            res.status(500).end();
+        })
+});
+
+app.get("/team/:teamId", (req, res) => {
+    data.getTeamById(req.params.teamId).then((data) => {
+            if (data.length > 0) {
+                res.json(data);
+            } else {
+                res.status(404).end();
+            }
+        })
+        .catch((err) => {
+            res.status(500).end();
+        })
+});
+
+app.get("/team-raw/:teamId", (req, res) => {
+    data.getTeamByIdRaw(req.params.teamId).then((data) => {
+            if (data.length > 0) {
+                res.json(data);
+            } else {
+                res.status(404).end();
+            }
+        })
+        .catch((err) => {
+            res.status(500).end();
+        })
+});
+
+app.put("/team/:teamId", (req, res) => {
+    data.updateTeamById(req.params.teamId, req.body).then((data) => {
+            res.json({ "message": "Team " + data + " updated successfully" });
+        })
+        .catch((err) => {
+            res.status(500).end();
+        })
 });
 
 app.post("/teams", (req, res) => {
-    
-    data.addTeam(req.body).then((data)=>{
-        res.json({"message": "Team " + data + " added successfully"});
-    })
-    .catch((err)=>{
-        res.status(500).end();
-    })
+
+    data.addTeam(req.body).then((data) => {
+            res.json({ "message": "Team " + data + " added successfully" });
+        })
+        .catch((err) => {
+            res.status(500).end();
+        })
 });
 
 ////////////////////
@@ -216,10 +246,10 @@ app.use((req, res) => {
 
 // Connect to the DB and start the server
 
-data.connect().then(()=>{
-    app.listen(HTTP_PORT, ()=>{console.log("API listening on: " + HTTP_PORT)});
-})
-.catch((err)=>{
-    console.log("unable to start the server: " + err);
-    process.exit();
-});
+data.connect().then(() => {
+        app.listen(HTTP_PORT, () => { console.log("API listening on: " + HTTP_PORT) });
+    })
+    .catch((err) => {
+        console.log("unable to start the server: " + err);
+        process.exit();
+    });
